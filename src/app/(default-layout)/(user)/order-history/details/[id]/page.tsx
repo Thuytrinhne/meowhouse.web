03a9dfcbb74c2d9f1c5dc1912e6f9a0a26b19045
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-04-12 14:39:38 +0700

    feat(orders) fix vercel deploy

--- src/app/(default-layout)/(user)/order-history/details/[id]/page.tsx
+++ src/app/(default-layout)/(user)/order-history/details/[id]/page.tsx
@@ -5,44 +5,7 @@ import { toast } from "react-toastify";
 import { useParams } from "next/navigation";
 import OrderDetails from "@/components/(order)/order-details";
 import { putData } from "@/utils/functions/client";
-
-interface OrderProduct {
-  product_hashed_id?: string;
-  product_id: string;
-  variant_id: string;
-  quantity: number;
-  unit_price: number;
-  discount_percent: number;
-  product_name: string;
-  product_img: string;
-  variant_name: string;
-  variant_img: string;
-}
-
-interface OrderBuyer {
-  name: string;
-  phone_number: string;
-  address: {
-    province: string;
-    district: string;
-    ward: string;
-    street: string;
-  };
-}
-
-interface Order {
-  _id: string;
-  order_id: string;
-  user_id: string;
-  order_products: OrderProduct[];
-  order_buyer: OrderBuyer;
-  order_note: string;
-  shipping_cost: number;
-  final_cost: number;
-  order_status: "unpaid" | "delivering" | "delivered" | "canceled";
-  createdAt: string | null;
-  order_id_hashed: string;
-}
+import { Order } from "@/types/order";
 
 interface OrderResponse {
   status: number;
